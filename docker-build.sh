fa773a2a3394f1f0a6be88b650a23357282c769b
chuonghuynh <minhchuong.itus@gmail.com>
2019-03-28 20:06:03 +0700

    [UPDATE] Add latest tag to build script

--- docker-build.sh
+++ docker-build.sh
@@ -1,1 +1,1 @@
-docker build -t asia.gcr.io/a2ds-235802/lr-classifier:$1 -f ./dockers/Dockerfile .
+docker build -t asia.gcr.io/a2ds-235802/lr-classifier:$1 -t asia.gcr.io/a2ds-235802/lr-classifier:latest -f ./dockers/Dockerfile .
--- docker-gpu-build.sh
+++ docker-gpu-build.sh
@@ -1,1 +1,1 @@
-docker build -t asia.gcr.io/a2ds-235802/lr-classifier-gpu:$1 -f ./dockers/Dockerfile-gpu .
+docker build -t asia.gcr.io/a2ds-235802/lr-classifier-gpu:$1 -t asia.gcr.io/a2ds-235802/lr-classifier-gpu:latest -f ./dockers/Dockerfile-gpu .
