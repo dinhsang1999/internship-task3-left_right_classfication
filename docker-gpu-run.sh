477f5c75ccab55dd8ecd780c1966d52691065aac
chuonghuynh <minhchuong.itus@gmail.com>
2019-03-29 17:15:52 +0700

    [UPDATE] Share volume script

--- docker-gpu-run.sh
+++ docker-gpu-run.sh
@@ -1,1 +1,1 @@
-docker run --runtime=nvidia -p $1:5000 -d asia.gcr.io/a2ds-235802/lr-classifier-gpu -v images:/images
+docker run --runtime=nvidia -p $1:5000 -v a2ds-volume:/images -d asia.gcr.io/a2ds-235802/lr-classifier-gpu
